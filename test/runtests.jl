9ea48bf9d70c5c9d382e664190aded175b436f82
Rob Blackwell <rob@robblackwell.com>
2017-12-18 16:19:07 +0000

    Include sample data

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,18 +3,12 @@ using Base.Test
 
 # EK60 file
 
-sample = joinpath(dirname(@__FILE__),
-                  "data/ek60/jr16003/ek60-sample.raw")
-
-_datagrams = collect(datagrams(sample))
+_datagrams = collect(datagrams(EK60_SAMPLE))
 
 @test length(_datagrams) == 4693
 
 # EK80 file
 
-sample = joinpath(dirname(@__FILE__),
-                  "data/ek80/EK80_Example_Data_01/EK80_SimradEcho_WC381_Sequential-D20150513-T090935.raw")
-
-_datagrams = collect(datagrams(sample))
+_datagrams = collect(datagrams(EK80_SAMPLE))
 
 @test length(_datagrams) == 461
