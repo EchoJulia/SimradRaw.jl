cfb26b33cac47f563003c8a339c7136ff2c849c7
Rob Blackwell <rob@robblackwell.com>
2018-07-17 09:34:07 +0100

    Support testing on 0.7

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,13 @@
 #!/usr/bin/env julia
 
 using SimradRaw
-using Base.Test
-
 using SimradEK60TestData
+
+@static if VERSION < v"0.7.0-DEV.2005"
+    using Base.Test
+else
+    using Test
+end
 
 # EK60 file
 
